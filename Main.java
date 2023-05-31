be9ed46b953fd837edc900f5ac0bfa72337da9a6
jezrreloriginal <124934158+jezrreloriginal@users.noreply.github.com>
2023-05-31 16:23:38 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+ int x = 5;
+
+    Main Obj1 = new Main();
+    Main Obj2 = new Main();
+    System.out.println(Obj1.x);
+    System.out.println(Obj2.x);
+  }
+
+ 
+}  Main myObj = new Main();
